78d1d00cfb0d89d2d03f49b9f8b933a8537bce2a
Parsa Frahani <90212907+parsaFarahani84@users.noreply.github.com>
2022-10-20 22:40:18 +0330

    ....

--- app.js
+++ app.js
@@ -3,8 +3,17 @@
 const sidebar = document.querySelector(".sidebar");
 const ham = document.querySelector(".ham");
 const a = document.querySelectorAll(".a");
+const close = document.querySelector(".close");
 
 ham.addEventListener("click", function () {
+  if (sidebar.classList.contains("show-sidebar")) {
+    sidebar.classList.remove("show-sidebar");
+  } else {
+    sidebar.classList.add("show-sidebar");
+  }
+});
+
+close.addEventListener("click", function () {
   if (sidebar.classList.contains("show-sidebar")) {
     sidebar.classList.remove("show-sidebar");
   } else {
