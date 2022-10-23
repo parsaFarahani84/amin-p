cf7d16f78e0c7a972892d90626332033db6b3d8c
Parsa Frahani <90212907+parsaFarahani84@users.noreply.github.com>
2022-10-23 23:34:45 +0330

    ...

--- app.js
+++ app.js
@@ -30,15 +30,3 @@ a.forEach((as) => {
     }
   });
 });
-
-function sendEmail() {
-  Email.send({
-    Host: "smtp.mailtrap.io",
-    Username: "<Mailtrap username>",
-    Password: "<Mailtrap password>",
-    To: "recipient@example.com",
-    From: "sender@example.com",
-    Subject: "Test email",
-    Body: "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>",
-  }).then((message) => alert(message));
-}
